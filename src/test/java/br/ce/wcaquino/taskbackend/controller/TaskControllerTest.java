6e2c838de669f53f0206933c918c1052a599c85c
Wagner Galvão <galvao.wagner@outlook.com.br>
2020-06-06 19:39:53 -0300

    Class 33: Criação da classe TaskControllerTest com Testes Unitários com Mockito

---
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -0,0 +1,79 @@
+package br.ce.wcaquino.taskbackend.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import br.ce.wcaquino.taskbackend.model.Task;
+import br.ce.wcaquino.taskbackend.repo.TaskRepo;
+import br.ce.wcaquino.taskbackend.utils.ValidationException;
+
+public class TaskControllerTest {
+	
+	@Mock
+	private TaskRepo taskRepo;
+
+	@InjectMocks
+	private TaskController controller;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefaSemDescricao() {
+		Task todo = new Task();
+//		todo.setTask("Descricao");
+		todo.setDueDate(LocalDate.now());
+		try {
+			controller.save(todo);
+			Assert.fail("Não gerou erro de ValidationException");
+		} catch (ValidationException e) {
+			assertEquals("Fill the task description", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefaSemData() {
+		Task todo = new Task();
+		todo.setTask("Descricao");
+//		todo.setDueDate(LocalDate.now());
+		try {
+			controller.save(todo);
+			Assert.fail("Não gerou erro de ValidationException");
+		} catch (ValidationException e) {
+			assertEquals("Fill the due date", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefaComDataPassada() {
+		Task todo = new Task();
+		todo.setTask("Descricao");
+		todo.setDueDate(LocalDate.now().minusDays(1));
+		try {
+			controller.save(todo);
+			Assert.fail("Não gerou erro de ValidationException");
+		} catch (ValidationException e) {
+			assertEquals("Due date must not be in past", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void deveSalvarTarefaComSucesso() throws ValidationException {
+		Task todo = new Task();
+		todo.setTask("Descricao");
+		todo.setDueDate(LocalDate.now());
+		controller.save(todo);
+		Mockito.verify(taskRepo).save(todo);
+	}
+}
